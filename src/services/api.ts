4d7e79a49b5241e314bf5d02212119d8bd497dbb
RaphaelMW <raphael@mundowap.com.br>
2023-02-08 17:32:21 -0300

    fix baseUrl

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import.meta.env.VITE_URL;
 
-const baseURL = import.meta.env.DEV ? "http://localhost:3333" : "http://localhost:3333/api";
+const baseURL = import.meta.env.DEV ? "http://localhost:3333" : "api";
 
 export const api = axios.create({
   baseURL: baseURL,
